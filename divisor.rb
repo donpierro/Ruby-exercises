3b84df5314aaee0b67b03caf4296dffe673af038
Piotr <stecpiotr1991@gmail.com>
2015-01-14 10:44:45 +0100

    added function to sum divisors for inputs

---
+++ divisor.rb
@@ -0,0 +1,10 @@
+def divisor a
+	return [] if a.empty?
+	a.map { |val| divisor_sum(val) }.uniq
+end
+
+
+def divisor_sum a
+	return 0 if (a <= 1 || a > 500000)
+	(1..a-1).select { |val| a % val == 0 }.inject(:+)
+end
---
+++ divisor_spec.rb
@@ -0,0 +1,15 @@
+require "./divisor.rb"
+
+describe "divisor" do
+	it { expect(divisor([3])).to eql [1] }
+	it { expect(divisor([3, 20])).to eql [1, 22] }
+	it { expect(divisor([])).to eql [] }
+	it { expect(divisor([3, 2, 10, 20])).to eql [1, 8, 22] }
+	it { expect(divisor([3, 2, 10, 20, 3, 2])).to eql [1, 8, 22] }
+end
+
+describe "divisor_sum function" do
+	it { expect(divisor_sum(20)).to eql 22 }
+	it { expect(divisor_sum(3)).to eql 1 }
+	it { expect(divisor_sum(-10)).to eql 0 }
+end
