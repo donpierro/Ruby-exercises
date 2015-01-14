4cac73437281d2fe875f3547e20d2f33b4f751ae
Piotr <stecpiotr1991@gmail.com>
2015-01-14 12:10:47 +0100

    fixed task

--- divisor.rb
+++ divisor.rb
@@ -1,9 +1,9 @@
 def divisor a
-	a.empty? ? [] : a.map { |val| divisor_sum(val) }.uniq
+	a.empty? ? [] : a.map { |val| divisor_sum(val) }
 end
 
 
 def divisor_sum a
 	return 0 if (a <= 1 || a > 500000)
-	(1..a-1).select { |val| a % val == 0 }.inject(:+)
+	(1..a/2).select { |val| a % val == 0 }.inject(:+)
 end
--- divisor_spec.rb
+++ divisor_spec.rb
@@ -4,8 +4,8 @@ describe "divisor" do
 	it { expect(divisor([3])).to eql [1] }
 	it { expect(divisor([3, 20])).to eql [1, 22] }
 	it { expect(divisor([])).to eql [] }
-	it { expect(divisor([3, 2, 10, 20])).to eql [1, 8, 22] }
-	it { expect(divisor([3, 2, 10, 20, 3, 2])).to eql [1, 8, 22] }
+	it { expect(divisor([3, 2, 10, 20])).to eql [1, 1, 8, 22] }
+	it { expect(divisor([3, 2, 10, 20, 3, 2])).to eql [1, 1, 8, 22, 1, 1] }
 end
 
 describe "divisor_sum function" do
