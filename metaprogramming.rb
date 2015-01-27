45873389f32986d1014bfb561cd342c3ae28018c
Piotr <stecpiotr1991@gmail.com>
2015-01-27 10:25:42 +0100

    fixed specs

--- metaprogramming.rb
+++ metaprogramming.rb
@@ -6,26 +6,9 @@ class Task
 
   %w(new open done).each do |method|
     define_method(method + '?') { @status == method ? true : false }
-    alias_method (method + '!'), (method + '?')
   end
 
-  define_method('change_status') do |status|
-    @status = status
+  %w(new open done).each do |method|
+    define_method(method + '!') { @status = method }
   end
-
-  # def change_status(status)
-  #   @status = status
-  # end
 end
-
-task1 = Task.new('new')
-task2 = Task.new('open')
-
-puts task1.new?
-puts task1.open?
-
-puts task2.done?
-task2.change_status('done')
-puts task2.done!
-
-puts Task.new.send(:done?)
--- metaprogramming_spec.rb
+++ metaprogramming_spec.rb
@@ -1,28 +1,25 @@
 require './metaprogramming.rb'
 
 describe 'Task' do
-
-  context 'create new task without param in constructor' do
+  context 'create new task without status in constructor' do
     it { expect(Task.new.send(:new?)).to be true }
     it { expect(Task.new.send(:done?)).to be false }
   end
 
-  context 'create new tast' do
-    it 'returns task status' do
-      task = Task.new('open')
-      expect(task.open?).to be true
-    end
+  it 'returns true if status is open' do
+    task = Task.new('open')
+    expect(task.open?).to be true
+  end
 
-    it 'returns task status' do
-      task = Task.new('done')
-      expect(task.open?).to be false
-    end
+  it 'returns false if status is not done' do
+    task = Task.new('open')
+    task.done!
+    expect(task.open?).to be false
+  end
 
-    it 'returns task status' do
-      task = Task.new('new')
-      task.change_status('open')
-      expect(task.open?).to be true
-    end
-
+  it 'changes status to open' do
+    task = Task.new('new')
+    task.open!
+    expect(task.open?).to be true
   end
 end
